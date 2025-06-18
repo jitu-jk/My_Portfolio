cecd223d62fc6c05bc381af40d7c9dda350040d8
jitu-jk <jitesh62684@gmail.com>
2025-06-19 02:35:12 +0530

    Dynamic base path for Vercel & GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+   
+
+// base: process.env.NODE_ENV === 'production'
+//   ? '/My_Portfolio/'  // for GitHub Pages
+//   : '/',              // for local & Vercel
+
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-   
-
-base: process.env.NODE_ENV === 'production'
-  ? '/My_Portfolio/'  // for GitHub Pages
-  : '/',              // for local & Vercel
-
+  base: process.env.VERCEL ? '/' : '/My_Portfolio/', // ✅ detects Vercel or GitHub
   plugins: [react()],
 })
